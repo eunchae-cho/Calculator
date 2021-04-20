693e0fa63153d9f7664090ad6230efc1976a3bf9
Eumchae Cho <eumchaecho@Eunchaeui-MacBookPro.local>
2021-04-20 18:52:54 +0900

    ,

--- demo/src/main/java/com/example/demo/CalculatorController.java
+++ demo/src/main/java/com/example/demo/CalculatorController.java
@@ -19,8 +19,17 @@ public class CalculatorController {
     @ResponseBody 
     @PostMapping("/calculate")
     public String calculator(String formula) {
-        String[] formulaArr = formula.split(",");           // ','로 구분한 계산식을 배열로 담기
-        float temp = 0;                                       // 순서대로 연산을 하기 위해 임시로 담는 값
+        String[] formulaArr = formula.split(",");   // ','로 구분한 계산식을 배열로 담기
+        float temp = 0;                             // 순서대로 연산을 하기 위해 임시로 담는 값
+
+        // formulaArr의 두번째 인덱스가 부호가 아닐 때 한 칸씩 앞으로 자리 이동
+        // 이럴 경우 처음 인덱스 값이 전의 계산된 결과값이기 때문 [연산숫자][입력숫자1][부호][입력숫자2] => [입력숫자1][부호][입력숫자2]
+        if (!isOperator(formulaArr[1])) {
+            for(int i = 0; i < formulaArr.length; i++) {
+                formulaArr[i] = formulaArr[i+1];
+            }
+            formulaArr[formulaArr.length] = null;
+        }
 
         for (int i = 0; i < formulaArr.length; i++) {
             if (i % 2 == 0) {                               // 인덱스 값이 짝수일 때 숫자, 홀수일 때 부호
@@ -72,6 +81,15 @@ public class CalculatorController {
     // 나누기
     public float divide(float firstNumber, float nextNumber) {
         return firstNumber / nextNumber;
+    }
+
+    // 문자열이 부호인지 아닌지 판단 
+    public boolean isOperator(String str) {
+        if (str.equals("+") || str.equals("-")
+            || str.equals("x") || str.equals("÷")) {
+                return true;
+        }
+        return false;
     }
 }
 
