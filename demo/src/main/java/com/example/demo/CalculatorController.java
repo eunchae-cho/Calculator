c010f1c6ee56af6c90feb99941e18ce8ff94623a
eunchae-cho <dmsco8981@gmail.com>
2021-04-23 18:07:28 +0900

    ,

--- demo/src/main/java/com/example/demo/CalculatorController.java
+++ demo/src/main/java/com/example/demo/CalculatorController.java
@@ -24,119 +24,131 @@ public class CalculatorController {
     @ResponseBody 
     @PostMapping("/calculate")
     public String calculator(String formula) {
-        String[] formulaArr = formula.split(",");   // ','로 구분한 계산식을 배열로 담기
-//         float temp = 0;                             // 순서대로 연산을 하기 위해 임시로 담는 값
-        List<String> numberList = new LinkedList<>();
-        List<String> operatorList = new LinkedList<>();
 
-        checkSecondIndexIsBlank(formulaArr);
-        checkSecondIndexIsOperator(formulaArr);
-
-        // formulaArr의 인덱스가 짝수일 때 숫자, 홀수일 때 부호을 구분하고
-        // 각각의 리스트를 생성
+        // ','로 구분한 계산식을 배열로 담기
+        String[] formulaArr = formula.split(",");
+        
+        // formulaArr 배열을 리스트로 담기
+        List<String> formulaList = new LinkedList<>();
         for (int i = 0; i < formulaArr.length; i++) {
-            if (i % 2 == 0) {
-                numberList.add(formulaArr[i]);
-            } else {
-                operatorList.add(formulaArr[i]);
-                // for(int j = 0; j < operatorList.size(); j++) {
-                //     if (operatorList.get(j).equals(null)) {
-                //         operatorList.remove(j);
-                //     }
-                // }
-            }
+            formulaList.add(formulaArr[i]);
         }
 
-        System.out.println();
-        Iterator<String> iterator = numberList.iterator();
-        System.out.println("numberList");
-        while (iterator.hasNext()) {
-            System.out.print(iterator.next()+ " ");
-        }
-        System.out.println();
-        Iterator<String> iterator2 = operatorList.iterator();
-        System.out.println("operatorList");
-        while (iterator2.hasNext()) {
-            System.out.print(iterator2.next()+ " ");
-        }
+        formulaArr = checkSecondIndexIsBlank(formulaArr);
+        System.out.println("formulaArr after checkSecondIndexIsBlank: " + Arrays.toString(formulaArr));
+        formulaArr = checkSecondIndexIsOperator(formulaArr);
+        System.out.println("formulaArr after checkSecondIndexIsOperator: " + Arrays.toString(formulaArr));
 
+        
 
+        // 확인용
+        System.out.print("formulaList: ");
+        printList(formulaList);
+        
+        // formulaList을 바탕으로 연산된 값을 계속해서 저장할 tempList 생성
+        List<String> tempList = new LinkedList<>();
+        tempList = formulaList;
+        int size = formulaList.size();
         float result = 0;
-        for (int i = 0; i < operatorList.size(); i++) {
-            if (operatorList.get(i).equals("x")) {
-                result = Float.parseFloat(numberList.get(i)) * Float.parseFloat(numberList.get(i + 1));
-                numberList.add(i, String.valueOf(result));
-                numberList.remove(i + 1);
-                operatorList.remove(i);
-            } else if (operatorList.get(i).equals("÷")) {
-                result = Float.parseFloat(numberList.get(i)) / Float.parseFloat(numberList.get(i + 1));
-                numberList.add(i, String.valueOf(result));
-                numberList.remove(i + 1);
-                operatorList.remove(i);
-            } else if (operatorList.get(i).equals("+")) {
-                result = Float.parseFloat(numberList.get(i)) + Float.parseFloat(numberList.get(i + 1));
-                numberList.add(i, String.valueOf(result));
-                numberList.remove(i + 1);
-                operatorList.remove(i);
-            } else if (operatorList.get(i).equals("-")) {
-                result = Float.parseFloat(numberList.get(i)) - Float.parseFloat(numberList.get(i + 1));
-                numberList.add(i, String.valueOf(result));
-                numberList.remove(i + 1);
-                operatorList.remove(i);
+        
+        // tempList에 연산값으로 하나만 저장될 때까지
+        while(tempList.size() != 1) {
+
+            // 곱하기
+            for (int i = 0; i < size; i++) {
+                if (formulaList.get(i).equals("x")) {
+                    result = Float.parseFloat(formulaList.get(i - 1)) * Float.parseFloat(formulaList.get(i + 1));   // ..[숫자1][x][숫자2].. 양쪽의 숫자 연산
+                    tempList.set(i, String.valueOf(result));
+                    System.out.println("(x)tempList.set 이후");
+                    printList(tempList);
+                    tempList.remove(i - 1);     // ..[숫자1][결과값][숫자2].. 에서 [숫자1]을 지움
+                    System.out.println("(x)tempList.remove(i-1) 이후");
+                    printList(tempList);
+                    tempList.remove(i);         // .. [결과값][숫자2].. 에서 [숫자2]를 지움
+                    System.out.println("(x)tempList.remove(i) 이후");
+                    printList(tempList);
+
+                    size = tempList.size();     // 연산된 리스트의 길이 대입 
+
+                } else {
+                    continue;
+                }
+            }
+
+            // 나누기
+            for (int i = 0; i < size; i++) {
+                if (formulaList.get(i).equals("÷")) {
+                    result = Float.parseFloat(formulaList.get(i - 1)) / Float.parseFloat(formulaList.get(i + 1)); 
+                    tempList.set(i, String.valueOf(result));
+                    System.out.println("(÷)tempList.set 이후");
+                    printList(tempList);
+                    tempList.remove(i - 1);
+                    System.out.println("(÷)tempList.remove(i-1) 이후");
+                    printList(tempList);
+                    tempList.remove(i);
+                    System.out.println("(÷)tempList.remove(i) 이후");
+                    printList(tempList);
+
+                    size = tempList.size();
+
+                } else {
+                    continue;
+                }
+            }
+    
+            // 더하기
+            for (int i = 0; i < size; i++) {
+                if (formulaList.get(i).equals("+")) {
+                    result = Float.parseFloat(formulaList.get(i - 1)) + Float.parseFloat(formulaList.get(i + 1)); 
+                    tempList.set(i, String.valueOf(result));
+                    System.out.println("(+)tempList.set 이후");
+                    printList(tempList);
+                    tempList.remove(i - 1);
+                    System.out.println("(+)tempList.remove(i-1) 이후");
+                    printList(tempList);
+                    tempList.remove(i);
+                    System.out.println("(+)tempList.remove(i) 이후");
+                    printList(tempList);
+
+                    size = tempList.size();
+
+                } else {
+                    continue;
+                }
+            }
+
+            // 빼기
+            for (int i = 0; i < size; i++) {
+                if (formulaList.get(i).equals("-")) {
+                    result = Float.parseFloat(formulaList.get(i - 1)) - Float.parseFloat(formulaList.get(i + 1)); 
+                    tempList.set(i, String.valueOf(result));
+                    System.out.println("(-)tempList.set 이후");
+                    printList(tempList);
+                    tempList.remove(i - 1);
+                    System.out.println("(-)tempList.remove(i-1) 이후");
+                    printList(tempList);
+                    tempList.remove(i);
+                    System.out.println("(-)tempList.remove(i) 이후");
+                    printList(tempList);
+
+                    size = tempList.size();
+
+                } else {
+                    continue;
+                }
             }
         }
-        return numberList.get(0);
-
-        // for (int i = 0; i < formulaArr.length; i++) {
-        //     if (i % 2 == 0) {                               // 인덱스 값이 짝수일 때 숫자, 홀수일 때 부호
-        //         if (i == 0) {
-        //             temp = Float.parseFloat(formulaArr[i]);
-        //         } else {
-        //             temp = compute(temp, Float.parseFloat(formulaArr[i]), formulaArr[i - 1]);  // (첫번째 연산값, 두번째 값, 부호)
-        //         }
-        //     }
-        // }
-        // return String.valueOf(temp);
+        return tempList.get(0);
     }
 
-    // 계산하기
-   public float compute(float firstNumber, float nextNumber, String operator) {
-       float result = 0;
-        switch(operator) {
-            case "+":  
-                result = add(firstNumber, nextNumber); 
-                break;
-            case "-":  
-                result = substract(firstNumber, nextNumber); 
-                break;
-            case "x":
-                result = multiply(firstNumber, nextNumber);
-                break;
-            case "÷":  
-                result = divide(firstNumber, nextNumber); 
-                break;
+
+    // 확인용 LinkedList 값 출력
+    public void printList(List<String> list) {
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next()+ ", ");
         }
-       return result;
-   }
-
-    // 더하기
-    public float add(float firstNumber, float nextNumber) {
-       return firstNumber + nextNumber;
-    }
-
-    // 빼기
-    public float substract(float firstNumber, float nextNumber) {
-        return firstNumber - nextNumber;
-    }
-
-    // 곱하기
-   public float multiply(float firstNumber, float nextNumber) {
-        return firstNumber * nextNumber;
-    }
-    
-    // 나누기
-    public float divide(float firstNumber, float nextNumber) {
-        return firstNumber / nextNumber;
+        System.out.println();
     }
 
     // 문자열이 부호인지 아닌지 판단 
@@ -148,14 +160,14 @@ public class CalculatorController {
         return false;
     }
 
-    // 누적 계산 시 두번째 인덱스가 "" 공백으로 넘어옴 => [전연산결과][][부호][숫자]
-    // 세번째 인덱스부터 한 칸씩 앞으로 이동한다 => [전연산결과][부호][숫자][null]
+    // 누적 계산 시 두번째 인덱스가 "" 공백으로 넘어옴
+    // 세번째 인덱스부터 한 칸씩 앞으로 이동한다
+    // [전연산결과][][부호][숫자] => [전연산결과][부호][숫자]
     public String[] checkSecondIndexIsBlank(String[] formulaArr) {
         if(formulaArr[1].equals("")) {
             for (int i = 1; i < formulaArr.length - 1; i++) {
                 formulaArr[i] = formulaArr[i + 1];
             }
-            //formulaArr[formulaArr.length - 1] = null;   // 맨 마지막 인덱스 값 null
             return Arrays.copyOf(formulaArr, formulaArr.length - 1);
         } else {
             return formulaArr;
@@ -170,10 +182,12 @@ public class CalculatorController {
             for(int i = 0; i < formulaArr.length - 1; i++) {
                 formulaArr[i] = formulaArr[i + 1];
             }
-            //formulaArr[formulaArr.length - 1] = null;
-            return Arrays.copyOf(formulaArr, formulaArr.length - 1);
+            String[] cutLastFormulaArr = Arrays.copyOf(formulaArr, formulaArr.length - 1);
+            System.out.println("cutLastFormulaArr: " + Arrays.toString(cutLastFormulaArr));
+            return cutLastFormulaArr;
+        } else {
+            return formulaArr;
         }
-        return formulaArr;
 
     }
 }
